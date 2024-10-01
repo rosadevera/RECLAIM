f3e6392e4729b97b0425a7aeac07db378ad5654c
rosadevera <dever014@newschool.edu>
2024-09-30 23:45:03 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// Select the image container
+const imageContainer = document.getElementById('image-container');
+
+// Add click event to hide the image on click
+imageContainer.addEventListener('click', () => {
+    imageContainer.classList.add('hidden');
+});
